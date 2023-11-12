ed3ca2c57a19024a45b2176d9b95233876db46c9
Jakob Danninger <68190922+jdanninger@users.noreply.github.com>
2023-11-12 18:48:15 -0500

    can add new searches but no validation

--- CCW-Frontend/src/components/TrackingTable.vue
+++ CCW-Frontend/src/components/TrackingTable.vue
@@ -43,27 +43,135 @@
     </v-container>
     </v-card>
     <br>
-    <v-dialog width="80%">
+
+
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      width="1024"
+    >
       <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" text="Open Dialog"> </v-btn>
+        <v-btn
+          color="primary"
+          v-bind="props"
+        >
+          New Search
+        </v-btn>
       </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">New Search</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                :rules="[value => !!value || 'Required']"
+                  label="Class Code*"
+                  v-model="code"
+                ></v-text-field>
+              </v-col>
 
-      <template v-slot:default="{ isActive }">
-        <v-card title="Dialog">
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  :rules="[value => !!value || 'Required']"
+                  v-model="number"
+                  label="Class Number*"
+                ></v-text-field>
+              </v-col>
 
-            <v-btn
-              text="Close Dialog"
-              @click="isActive.value = false"
-            ></v-btn>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Class Section"
+                  v-model="section"
+                ></v-text-field>
+              </v-col>
+              
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+              <v-autocomplete
+                  :items="['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']"
+                  label="Days Offered"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+              <v-autocomplete
+                  :items="['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm',]"
+                  label="Starts on or after"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+              <v-autocomplete
+                  :items="['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm',]"
+                  label="End on or before"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+              
+
+            
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="submit"
+          >
+            Add
+          </v-btn>
         </v-card-actions>
-        </v-card>
-      </template>
+      </v-card>
     </v-dialog>
+  </v-row>
+
+
+
+
+
+
+
+
+    
    
   </template>
 
@@ -83,6 +191,11 @@
     export default {
       data () {
         return {
+          code: '',
+          number: '',
+          section: '',
+
+          dialog: false,
           courses: [
             {
               code: 'CSDS',
@@ -122,9 +235,18 @@
         this.courses = this.courses.filter((obj) => {
           return obj != item
         })
-        
-
-      }
+      },
+      async submit(event){
+        this.courses.push(
+          {
+            code: this.code,
+              number: this.number,
+              section: this.section,
+              days: "M W F",
+              time: "8:30 am"
+          }
+        )
+      },
     }
     }
   </script>
