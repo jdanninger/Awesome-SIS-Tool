3455aed4ee81722cf47de279540d411f5dfa3e48
georgia-martinez <gcm49@case.edu>
2023-11-14 11:07:37 -0500

    Added unit tests for ExcelReader

--- backend/tests/test_scraper.py
+++ backend/tests/test_scraper.py
@@ -8,8 +8,11 @@ class TestSISScraper(unittest.TestCase):
     def setUp(self):
         self.scraper = SISScraper(headless=False)
 
+    def test_headless(self):
+        scraper = SISScraper(headless=True)
+
     def test_search(self):
         self.scraper.search("Fall 2023", "CSDS 132")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
