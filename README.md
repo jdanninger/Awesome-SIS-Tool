b857a627d29497c9ba2e2a747002ff510955a99b
Neha0315 <nehapanduri@gmail.com>
2023-11-27 18:40:01 -0500

    DB connection
    The flask connection to database

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ npm run dev
 ```
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install requirements.txt
+pip3 install -r requirements.txt
 ```
 
 # Running Tests
--- backend/src/app.py
+++ backend/src/app.py
@@ -2,17 +2,20 @@
 from flask import Flask, render_template
 import pymysql
 
-db = pymysql.connect(host="localhost", user="root", password="Neha0315!", database="case-coursewatch")
 
-app = Flask(__name__)
 
-@app.route("/")
-def someName():
-    cursor = db.cursor()
-    sql = "SELECT * FROM TABLE"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return render_template("index.html", results=results)
+#app = Flask(__name__)
+
+#@app.route("/")
+#def someName():
+   
+    #return render_template("index.html", results=results)
  
 if __name__ == "__main__":
-    app.run(debug=True)
+   # app.run(debug=True)
+    db = pymysql.connect(host="localhost", user="root", password="Neha0315!", database="case-coursewatch")
+    cursor = db.cursor()
+    sql = "SELECT * FROM `case-coursewatch`.CourseInfo;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
