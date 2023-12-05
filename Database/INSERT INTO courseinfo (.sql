02b356b147603c2ee61f19fc3cc8707ab105f082
Neha0315 <nehapanduri@gmail.com>
2023-12-05 16:29:00 -0500

    DB setup
    new sql code

---
+++ Database/INSERT INTO courseinfo (.sql
@@ -0,0 +1,31 @@
+
+INSERT INTO courseinfo (
+    course_code,
+    course_name,
+    professor,
+    days,
+    time
+  )
+VALUES (
+    'CSDS 101',
+    'The Digital Revolution',
+    'H.Connamacher',
+    'MWF',
+    '12:30-1:30pm'
+  );
+
+
+  INSERT INTO courseinfo (
+      course_code,
+      days,
+      time,
+      course_name,
+      professor
+    )
+  VALUES (
+      'course_code: ',
+      'days:character varying',
+      'time:character varying',
+      'course_name:character varying',
+      'professor:character varying'
+    );
---
+++ connection4.session.sql
@@ -0,0 +1,45 @@
+
+
+
+
+
+--Already created tables 
+/*CREATE TABLE searchHistory (
+    username VARCHAR(20) NOT NULL, 
+    course_ID INT NOT NULL, 
+    date_searched VARCHAR(10) NOT NULL, 
+    PRIMARY KEY(username, course_ID), 
+    FOREIGN KEY(username) REFERENCES Login(username), 
+    FOREIGN KEY(course_ID) REFERENCES CourseInfo(course_ID)
+);
+
+
+*/
+
+/*create table Login(
+    username VARCHAR(20) NOT NULL, 
+    password VARCHAR(20) NOT NULL, 
+    email VARCHAR(20) NOT NULL, 
+    PRIMARY KEY(username)
+);
+*/
+
+
+/*create table CourseInfo(
+    course_ID SERIAL PRIMARY KEY, 
+    course_code VARCHAR(20) NOT NUll, 
+    course_name VARCHAR(20) NOT NULL, 
+    professor VARCHAR(20) NOT NULL, 
+    days VARCHAR(10) NOT NULL,
+    time VARCHAR(20) NOT NULL
+);
+*/
+
+/*create table trackedCourses(
+    username VARCHAR(20) NOT NULL, 
+    course_ID INT NOT NULL, 
+    PRIMARY KEY(username, course_ID), 
+    FOREIGN KEY(username) REFERENCES login(username), 
+    FOREIGN KEY(course_ID) REFERENCES courseinfo(course_ID)
+);
+*/
