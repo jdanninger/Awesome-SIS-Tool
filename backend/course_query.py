e9d0d4866a8566f3d89aab24533c984e1a489bac
georgia-martinez <gcm49@case.edu>
2023-12-07 11:18:26 -0500

    Deleted unnecessary CourseQuery class

--- backend/course_query.py
+++
@@ -1,9 +0,0 @@
-class CourseQuery:
-    def __init__(self, code, num, name, section, days, time, prof):
-        self.code = code
-        self.num = num
-        self.name = name
-        self.section = section
-        self.days = days
-        self.time = time
-        self.prof = prof
