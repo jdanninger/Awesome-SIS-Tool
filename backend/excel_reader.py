0ca277597e33333d470eddc4d1850d5d9f4d7f39
georgia-martinez <gcm49@case.edu>
2023-12-06 22:54:33 -0500

    Refactored filtering so it actually works now

--- backend/excel_reader.py
+++ backend/excel_reader.py
@@ -20,31 +20,31 @@ class ExcelReader:
         df = pd.read_excel(csv_path)
         os.remove(csv_path)
 
-        mask = pd.Series(False, index=df.index)
+        filter = pd.Series(False, index=df.index)
 
-        # course dict keys mapped to the downloaded csv column names
-        self.csv_headers = {
-            "course_code": "SUBJECT",
-            "course_num": "CATALOG_NBR",
-            "course_name": "CW_CLASS_TITLE",
-            "section": "CLASS_SECTION",
-            "days": "CLASS_MTG_DAYS",
-            "time": "CW_CLASS_MTG_TIMES",
-            "professor": "INSTR_NAME"
-        }
+        if course["course_code"]:
+            filter = filter | (df["SUBJECT"] == course["course_code"])
 
-        for key in course.keys():
-            if course[key] is not None:
-                if key == "course_id" or key == "user_name":
-                    continue
+        if course["course_num"]:
+            filter = filter & (df["CATALOG_NBR"] == int(course["course_num"]))
 
-                if key != "name" or key != "professor":
-                    mask |= (course[key] == df[self.csv_headers[key]])
+        if course["course_name"]:
+            filter = filter & df["CW_CLASS_TITLE"].apply(lambda x: str(x).lower()).str.contains(course["course_name"].lower())
 
-        df = df[mask]
+        if course["section"]:
+            filter = filter & (df["CLASS_SECTION"] == int(course["section"]))
 
-        df = df[df["INSTR_NAME"].apply(lambda x: str(x).lower()).str.contains(course["professor"].lower())]
-        df = df[df["CW_CLASS_TITLE"].apply(lambda x: str(x).lower()).str.contains(course["course_name"].lower())]
+        if course["days"]:
+            filter = filter & (df["CLASS_MTG_DAYS"].str.strip() == course["days"].strip())
+
+        if course["time"]:
+            filter = filter & (df["CW_CLASS_MTG_TIMES"] == course["time"])
+
+        if course["professor"]:
+            filter = filter & df["INSTR_NAME"].apply(lambda x: str(x).lower()).str.contains(course["professor"].lower())
+
+        df = df[filter]
+        print(df)
 
         for _, row in df.iterrows():
             if "Open" in str(row["ENRL_STATUS"]):
